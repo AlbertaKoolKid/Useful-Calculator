2f3f1c3dd85019ad923728039772e599a7fd0899
AlbertaKoolKid <90733258+AlbertaKoolKid@users.noreply.github.com>
2024-03-27 18:19:44 -0600

    Updated the print lines to use the clearline function to help with readability

--- Useful Calculator.py
+++ Useful Calculator.py
@@ -18,92 +18,123 @@ def clearline(n):
         print()
 
 def surarea():
-    """This is the function for calculating surface area. 
-    
-    This function will calculate the surface area of a handful of different 
-    objects. It is self contained and doesn't referenct outside functions.
     """
-    
-    print("\n\n\nHere is a list of currently coded shapes.\n"
-          "'rectangle' 'circle' 'triangle' \n\n")
+    This is the function for calculating surface area. 
+    This function will calculate the surface area of a handful of different 
+    objects. It uses the above function to help readability for code.
+    """
+    clearline(3)
+    print("Here is a list of currently coded shapes.\n"
+          "'rectangle' 'circle' 'triangle'...")
+    clearline(3) 
     shape = input("What is the shape you are trying to calculate?   ")
-    
-    if shape == "rectangle": 
-        l = int(input("What is the length?   "))
-        h = int(input("What is the height?   "))
+    clearline(3)
+
+    if shape == "rectangle" or shape == "rec": 
+        l = float(input("What is the length?   "))
+        h = float(input("What is the height?   "))
         sa = l*h
-        print("\n\nThe surface area of the rectangle is " 
+        clearline(3)
+        print("The surface area of the rectangle is " 
               + str(sa) 
-              + " units squared.\n\n")        
-    
-    elif shape == "circle":                                                                         
+              + " units squared.")
+        clearline(3)
+
+    elif shape == "circle" or shape =="cic":
         pi = math.pi
-        r = int(input("What is the radius of the circle?   "))
+        r = float(input("What is the radius of the circle?   "))
         sa = pi*(r**2)
-        print("\n\nThe surface area of the circle is " 
+        clearline(3)
+        print("The surface area of the circle is " 
               + str(sa) 
-              + " units squared.\n\n")           
+              + " units squared.")
+        clearline(3)
+
     elif shape == "triangle":
-        b = int(input("What is the base of the triangle?   "))
-        h = int(input("What is the height of the triangle?   "))
+        b = float(input("What is the base of the triangle?   "))
+        h = float(input("What is the height of the triangle?   "))
         a = b*h/2
-        print("\n\nThe surface area of the triangle is "
+        clearline(3)
+        print("The surface area of the triangle is "
               + str(a) 
-              + " units squared.\n\n")            
+              + " units squared.")
+        clearline(3) 
+ 
     else:
-        print("\n\nThis shape is not added yet please let the author know."
-              "\n\n")
+        clearline(3)
+        print("This shape is not added yet please let the author know.")
+        clearline(3)
         return
 
 def percent():
-    print("\n\n\nThis works by taking a first number which is the "
+    clearline(3)
+    print("This works by taking a first number which is the "
           "position you are at,\n"
-          "and a second number which is the total amount of places.\n\n"
-          "For example if you entered the numbers 5 and 15 you would"
+          "and a second number which is the total amount of places.")
+    clearline(3)
+    print("For example if you entered the numbers 5 and 15 you would"
           "be shown that \n"
-          "you are 33 percent through and the ratio is 5/15.\n\n\n")
-    x = int(input("What is the number for the current positon?   "))
-    y = int(input("What is the number for the total?   "))
+          "you are 33 percent through and the ratio is 5/15.")
+    clearline(3) 
+    x = float(input("What is the number for the current positon?   "))
+    y = float(input("What is the number for the total?   "))
     z = x*100/y
-    print("\n\n\nYou are currently "
+    clearline(3)
+    print("You are currently "
           +str(z)
           + " percent through.\nThe ratio is " 
           +str(x)
           + "/" 
           +str(y)
-          + ".\n\n\n")
+          + ".")
+          clearline(3)
     return
 
 def volume():
-    print("\n\n\nHere are a list of currently coded shapes.\n'cube'\n\n\n")
+    clearline(3)
+    print("Here are a list of currently coded shapes.\n'cube'")
+    clearline(3)
     shape = input("What is the shape you are trying to calculate?   ")
     if shape == "cube":
-        l = int(input("What is your side length?   "))
+        l = float(input("What is your side length?   "))
         v = l**3
-        print("\n\nThe volume of this cube is " + str(v) +	 " units.\n\n")
+        clearline(3)
+        print("The volume of this cube is " + str(v) +	 " units.")
+        clearline(3)
+ 
     else:
-        print("\n\nNot a valid option\n\n")
+        clearline(3)
+        print("Not a valid option")
+        clearline(3)
 
 def fahrenheit():
-    cel = int(input("What is the current degrees Celsius?   "))
+    cel = float(input("What is the current degrees Celsius?   "))
     fah = (cel*9/5)+32
-    print("\n\nIt is currently "+str(fah)+" degrees Fahrenheit.\n\n")
-    return
+    clearline(3)
+    print("It is currently "+str(fah)+" degrees Fahrenheit.")
+    clearline(3)
 
 def celsius():
-    fah = int(input("What is the current degrees Fahrenheit?   ")) #gets current teperature and sets in be an integer
-    cel = (fah-32)*5/9 #formula to calculate celcius from fahrenheit
-    print("\n\nIt is currently "+str(cel)+" degrees Celsius\n\n") #prints the string with the interjection of the output variable as a string
-    return
+    fah = float(input("What is the current degrees Fahrenheit?   "))
+     #gets current teperature and sets in be an integer
+    
+    cel = (fah-32)*5/9 
+    #formula to calculate celcius from fahrenheit
+
+    clearline(3)
+    print("It is currently "+str(cel)+" degrees Celsius.")
+    #prints the string with the interjection of the output variable as a string
+    clearline(3)
 
 def fractodec(): #not an easy formula. 
     print ("not yet implemented")
-    return
 
-def startup():
-    """Here lies the main menu
-    
+def startup(running):
+
     """
+    Here lies the main menu
+    """
+
     clearline(10)
     print("Welcome to the Useful Calculator.\n"
           "With this program you will be able to calculate many different "
@@ -115,13 +146,21 @@ def startup():
           "Type 'celsius' to calculate Celsius from Fahrenheit.\n"
           "Type 'end' to quit the calculator and retrieve the save data.\n"
           )
-    running = True
+          
     while running == True:
         clearline(3)
-        funInput1 = input("What would you like to do?   ") #asking for input
-        funInput = funInput1.lower() #forcing the inputed string to become all lowercase
-        if funInput == "percentage" or funInput == "per": #check if input is equal to the string if so run the function
-            percent()             #run the function
+        
+        #asking for input
+        funInput1 = input("What would you like to do?   ") 
+        
+        #forcing the inputed string to become all lowercase
+        funInput = funInput1.lower()
+        
+        #check if input is equal to the string if so run the function
+        if funInput == "percentage" or funInput == "per":
+            #run the function
+            percent()
+        
         elif funInput == "surface area" or funInput == "sa":
             surarea()
         elif funInput == "fahrenheit" or funInput == "fah":
@@ -132,13 +171,16 @@ def startup():
             volume()
         elif funInput == "end" or funInput == "stop":
             running = False
-        else:                   #if the input does not match any valid strings it gets sent to here
-            print("\n\nUnfortunatly this is not a valid option or not implemented yet, please let the author know.\n\n")
+        else:
+        #if the input does not match any valid strings it gets sent to here
+            clearline(3)
+            print("Unfortunatly this is not a valid option or not implemented yet, please let the author know.")
+            clearline(3)
     
 #start of the main loop and non function based code		
-startup()
+startup(True)
 print()
 print("Thanks for using my calculator. Enjoy your day!")
-
+print("Written and maintained by Brady Clark-Doherty.")
 
 
